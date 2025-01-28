b381d6f3717f98c0645cec7a295e4c21ca24efce
ao donovan <a.o.donovan.music@gmail.com>
2025-01-28 11:53:39 -0500

    init commit

---
+++ src/main/java/com/example/rev_eshop/RevEshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.rev_eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevEshopApplication.class, args);
+	}
+
+}
