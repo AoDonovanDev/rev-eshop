24963b367aee59a2e58e5bdad4373584d67625bb
ao donovan <a.o.donovan.music@gmail.com>
2025-01-31 11:55:25 -0500

    stripe dependency added w/ stripe example

---
+++ src/main/java/com/example/rev_eshop/stripe/StripeExample.java
@@ -0,0 +1,31 @@
+package com.example.rev_eshop.stripe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.stripe.StripeClient;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.net.RequestOptions;
+import com.stripe.param.CustomerCreateParams;
+
+public class StripeExample {
+
+    public static void main(String[] args) {
+        StripeClient client = new StripeClient("sk_test_...");
+        CustomerCreateParams params =
+            CustomerCreateParams
+                .builder()
+                .setDescription("Example description")
+                .setEmail("test@example.com")
+                .setPaymentMethod("pm_card_visa")  // obtained via Stripe.js
+                .build();
+
+        try {
+            Customer customer = client.customers().create(params);
+            System.out.println(customer);
+        } catch (StripeException e) {
+            e.printStackTrace();
+        }
+    }
+}
